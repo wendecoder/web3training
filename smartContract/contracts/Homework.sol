18e82b82f2fb8770027fc5da4e5e4336e7334bb8
RedietNegash <redietnegash9@gmail.com>
2024-07-02 15:28:41 +0300

    optmized the code to reduce the gas consumption

--- smartContract/contracts/Homework.sol
+++ smartContract/contracts/Homework.sol
@@ -1,3 +1,6 @@
+//Rediet Negash Group 3
+// Initial Gas Consumption = 21149219n
+// Final Gas Consumption = 18451168n
 pragma solidity ^0.8.0;
 
 contract InefficientExample {
@@ -5,47 +8,57 @@ contract InefficientExample {
     uint256 public itemCount;
     mapping(address => uint256) public balances;
     address[] public users;
+    mapping(address => bool) public userSet;
 
     function calculateTotalValue() public returns (uint256) {
-        totalValue = 0;
+        //totalValue = 0; It is not essential to set the value to 0 since it always starts from zero
         for (uint256 i = 0; i < users.length; i++) {
             totalValue += balances[users[i]];
         }
         return totalValue;
     }
 
+    //Instead of storing the user address on the array it is more efficent to use mapping and to check whether the user exists before storing or mapping the user address
     function addItem(uint256 value) public {
         balances[msg.sender] += value;
-        users.push(msg.sender);
-        itemCount++; 
+
+        if (!userSet[msg.sender]) {
+            userSet[msg.sender] = true;
+        }
     }
+    //use byte instead of string
+    bytes[] public items;
 
-    string[] public items;
+    //function addItemName(string memory itemName) public {
+    //  items.push(itemName);
+    //  }
 
-    function addItemName(string memory itemName) public {
-        items.push(itemName);
-    }
+    event ItemAdded(address indexed user, uint256 value, bytes itemName);
 
-    event ItemAdded(address indexed user, uint256 value, string itemName);
-
-    function addItemWithEvent(uint256 value, string memory itemName) public {
+    // This function is doing the same task as addItem function
+    /* function addItemWithEvent(uint256 value, string memory itemName) public {
         balances[msg.sender] += value;
         users.push(msg.sender);
         items.push(itemName);
         itemCount++;
         emit ItemAdded(msg.sender, value, itemName);
     }
+*/
 
     struct Product {
         uint256 id;
         uint256 price;
-        string name;
+        bytes name;
         bool available;
     }
-
     Product[] public products;
 
-    function addProduct(uint256 id, uint256 price, string memory name, bool available) public {
+    function addProduct(
+        uint256 id,
+        uint256 price,
+        bytes memory name,
+        bool available
+    ) public {
         products.push(Product(id, price, name, available));
     }
 }
