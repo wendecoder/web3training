cce1396781dba7a38af7998226bbe5312ef62cc3
Duresa Feyisa <101529984+duressa-feyissa@users.noreply.github.com>
2024-07-02 13:52:51 +0000

    :stethoscope: Optimize smart contracts

--- smartContract/contracts/Gas.sol
+++ smartContract/contracts/Gas.sol
@@ -1,59 +1,52 @@
 pragma solidity ^0.8.0;
 
-contract VeryInefficientContract {
+contract VeryEfficientContract {
     uint256 public totalSum;
     uint256 public count;
     uint256[] public data;
 
-    // This function does not use view or pure when it could
-    function getSum() public returns (uint256) {
+
+    function getSum() public view returns (uint256) {
         uint256 sum = 0;
         for (uint256 i = 0; i < data.length; i++) {
             sum += data[i];
-            totalSum += data[i]; // Unnecessary state change within the loop
         }
-        totalSum = sum; // Unnecessary state change
         return sum;
     }
 
-    // This function changes state unnecessarily within a loop
     function addData(uint256[] memory newData) public {
         for (uint256 i = 0; i < newData.length; i++) {
             data.push(newData[i]);
-            count++; // Unnecessary state change within a loop
         }
-        count += newData.length; // Another unnecessary state change
+        count += newData.length;
     }
 
-    // Inefficient storage of strings
     string public name;
     string[] public previousNames;
 
     function setName(string memory newName) public {
-        name = newName; // Inefficient storage
-        previousNames.push(newName); // Additional inefficient storage
+        name = newName;
+        previousNames.push(newName);
     }
 
-    // Poor usage of modifiers
-    modifier checkCount(uint256 number) {
-        require(number < count, "Number is too large");
+    modifier validIndex(uint256 index) {
+        require(index < data.length, "Index out of bounds");
         _;
     }
 
-    function getData(uint256 index) public checkCount(index) returns (uint256) {
+    function getData(uint256 index) public view validIndex(index) returns (uint256) {
         return data[index];
     }
 
-    // Inefficient packing of variables
     struct User {
         uint256 id;
-        uint256 balance;
         bool active;
+        uint256 balance;
     }
 
     User[] public users;
 
     function addUser(uint256 id, uint256 balance, bool active) public {
-        users.push(User(id, balance, active));
+        users.push(User(id, active, balance));
     }
 }
--- smartContract/contracts/Homework.sol
+++ smartContract/contracts/Homework.sol
@@ -1,40 +1,11 @@
 pragma solidity ^0.8.0;
 
-contract InefficientExample {
+contract EfficientExample {
     uint256 public totalValue;
     uint256 public itemCount;
     mapping(address => uint256) public balances;
-    address[] public users;
-
-    function calculateTotalValue() public returns (uint256) {
-        totalValue = 0;
-        for (uint256 i = 0; i < users.length; i++) {
-            totalValue += balances[users[i]];
-        }
-        return totalValue;
-    }
-
-    function addItem(uint256 value) public {
-        balances[msg.sender] += value;
-        users.push(msg.sender);
-        itemCount++; 
-    }
-
+    mapping(address => bool) public registeredUsers;
     string[] public items;
-
-    function addItemName(string memory itemName) public {
-        items.push(itemName);
-    }
-
-    event ItemAdded(address indexed user, uint256 value, string itemName);
-
-    function addItemWithEvent(uint256 value, string memory itemName) public {
-        balances[msg.sender] += value;
-        users.push(msg.sender);
-        items.push(itemName);
-        itemCount++;
-        emit ItemAdded(msg.sender, value, itemName);
-    }
 
     struct Product {
         uint256 id;
@@ -44,6 +15,31 @@ contract InefficientExample {
     }
 
     Product[] public products;
+
+    event ItemAdded(address indexed user, uint256 value, string itemName);
+
+    function calculateTotalValue() public view returns (uint256) {
+        return totalValue;
+    }
+
+    function addItem(uint256 value) public {
+        if (!registeredUsers[msg.sender]) {
+            registeredUsers[msg.sender] = true;
+            itemCount++;
+        }
+        balances[msg.sender] += value;
+        totalValue += value;
+    }
+
+    function addItemName(string memory itemName) public {
+        items.push(itemName);
+    }
+
+    function addItemWithEvent(uint256 value, string memory itemName) public {
+        addItem(value);
+        addItemName(itemName);
+        emit ItemAdded(msg.sender, value, itemName);
+    }
 
     function addProduct(uint256 id, uint256 price, string memory name, bool available) public {
         products.push(Product(id, price, name, available));
